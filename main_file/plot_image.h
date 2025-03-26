e6b12a9374bd74bfd3179f9da965a98ed08e9905
Yimin-Chu <chuyimin2023@outlook.com>
2025-03-26 11:05:57 -0400

    keyboard_animation

--- main_file/plot_image.h
+++ main_file/plot_image.h
@@ -2,7 +2,7 @@
 #define __PLOT_IMAGE_H__
 
 // 声明全局变量（在 plot_image.c 中定义）
-extern int pixel_buffer_start;
+extern volatile pixel_buffer_start;
 
 // 如果你打算把图像数组定义在别的文件，也可以在这里 extern
 extern short unsigned int background[76800];
@@ -11,7 +11,7 @@ extern short unsigned int tower1[42*26];
 extern short unsigned int intro[240*320];
 
 // 声明函数
-void plot_pixel(int x, int y, short int color);
+void plot_static_pixel(int x, int y, short int color);
 void plot_image_background(int x, int y);
 void erase_image_background(int x, int y);
 void plot_image_tower1(int x, int y);
